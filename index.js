c701b20a01b7cf69f94bd6c67f7149abc5bdd08b
Ricardo <landinezrichard@gmail.com>
2025-10-30 15:08:20 -0500

    feat: modelo Viaje, controlador paginas

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import router from "./routes/index.js";
+import db from "./config/db.js";
 
 // Crear la app
 const app = express();
+
+// Conectar a la base de datos
+db.authenticate()
+  .then(() => console.log("Base de datos conectada"))
+  .catch((error) => console.log("Error al conectar la base de datos: ", error));
 
 // Definir puerto
 const port = process.env.PORT || 4000;
