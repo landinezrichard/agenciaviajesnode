d1a1dc3a95f6271279c8f1f13d171b7bb9fb3d92
Ricardo <landinezrichard@gmail.com>
2025-11-04 15:24:37 -0500

    feat: bloque testimoniales, múltiples consultas BD en home

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -3,12 +3,19 @@ import { Testimonial } from "../models/Testimonial.js";
 
 const paginaInicio = async (req, res) => {
   // Consultar 3 viajes del modelo Viaje
+  const promiseDB = [];
+
+  promiseDB.push(Viaje.findAll({ limit: 3 }));
+  promiseDB.push(Testimonial.findAll({ limit: 3 }));
+
   try {
-    const viajes = await Viaje.findAll({ limit: 3 });
+    const resultado = await Promise.all(promiseDB);
+
     res.render("inicio", {
       pagina: "Inicio",
       clase: "home",
-      viajes,
+      viajes: resultado[0],
+      testimoniales: resultado[1],
     });
   } catch (error) {
     console.log("Error al obtener los viajes: ", error);
